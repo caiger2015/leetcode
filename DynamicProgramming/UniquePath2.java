ebd6449ae6bab77bcd4069e9b8b8ef94015bd5b4
caiger <caiger@163.com>
2015-06-09 10:59:35 +0800

    Path problems
    initialize the first row and the first element of each row！

---
+++ DynamicProgramming/UniquePath2.java
@@ -0,0 +1,34 @@
+package leetcode.dynamicProgramming;
+
+public class UniquePath2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static int uniquePath2(int[][] grid){
+        int m=grid.length;
+        int n=grid[0].length;
+        int[] res=new int[n];
+        if(grid[0][0]==1)
+            res[0]=0;
+        else 
+            res[0]=1;
+        for(int i=0;i<m;i++){
+        	//考虑限制条件可能影响到的所有地方
+        	//这里判定每排第一个的时候，还要考虑上一排的情况
+            if(grid[i][0]==1||res[0]==0)
+                res[0]=0;
+            else
+                res[0]=1;
+            for(int j=1;j<n;j++){
+                if(grid[i][j]==1)
+                    res[j]=0;
+                else
+                    res[j]=res[j]+res[j-1];
+            }
+        }
+        return res[n-1];
+	}
+
+}
